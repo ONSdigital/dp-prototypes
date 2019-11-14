203089dce41bc323f093571c942daf1d381cfed7
robchamberspfc <robchamberspfc@gmail.com>
2019-11-14 10:01:50 +0000

    finder and update stling of CMD list

--- prototypes/finder/load.js
+++ prototypes/finder/load.js
@@ -3,9 +3,11 @@ fetch("data/setup.json")
         return response.json();
     })
     .then(function (data) {
-        document.getElementById("title").innerHTML = data.title
-        document.getElementById("page-title").innerHTML = data.title
-        document.getElementById("description").innerHTML = data.description
+        document.getElementById("title").innerHTML = data.title;
+        document.getElementById("page-title").innerHTML = data.title;
+        document.getElementById("description").innerHTML = data.description;
+        document.getElementById("domain1").innerHTML = data.toplevellabel;
+        document.getElementById("domain2").innerHTML = data.toplevellabel;
 
         for (i = 0; i < data.toplevel.length; i++) {
             option = document.createElement("option")
